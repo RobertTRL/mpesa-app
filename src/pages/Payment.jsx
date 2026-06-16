d356cd34132c39ce2d17154b32909482f6a52a2f
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-16 16:16:14 +0300

    Added useState import on payment.jsx to solve rendering issue

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,6 +1,7 @@
 import Form from "../components/Form.jsx"
 import Header from "../components/Header.jsx"
 import Qrcode from "../components/Qrcode.jsx"
+import { useState } from "react"
 import '../styles/payment.css'
 
 export default function Payment(){
